1d85eaaea7ba48fddf3379afe95c0112ccbe0568
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2022-10-07 19:06:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*jshint esversion: 6 */
+$( document ).ready(function() {
+
+    $(function() {
+        $('.btn_more').click(function(){
+           $('.more_leng').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.more_leng a').click(function(){
+           $('.more_leng').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.first').click(function(){
+           $('.first_an').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.secound').click(function(){
+           $('.secound_an').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.third').click(function(){
+           $('.third_an').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.four').click(function(){
+           $('.four_an').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.five').click(function(){
+           $('.five_an').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.six').click(function(){
+           $('.six_an').toggleClass('show');
+        });
+    });
+    $(function() {
+        $('.seven').click(function(){
+           $('.seven_an').toggleClass('show');
+        });
+    });
+});
